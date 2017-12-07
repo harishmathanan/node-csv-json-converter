31ee4d998ccd2cab8b14bc2ed1060b5b183139b2
Navinachendra <navih@south32.net>
2017-12-07 15:21:48 +0800

    Refactor main function.

--- converter.js
+++ converter.js
@@ -7,42 +7,42 @@ const csvToJSON = (fileName) => {
   // get file content
   fs.readFile(path.join(__dirname, fileName), 'utf8', (err, data) => {
     if (err) {
-      console.log('ERROR reading the input CSV file.');
-      console.log(err.message);
+      console.log(`ERROR reading the input CSV file. \n ${err.message}`);
     }
 
-    // break file data into lines
+    // break content into data rows
     const lines = data.split('\n');
-    // first line is headers for JSON properties
+    // first row is headers for JSON properties
     const headers = lines[0].split(',');
 
-    // begin building JSON
-    let results = [];
+    const jsonContent = createJSON(lines, headers);
+    const jsonFilename = path.parse(fileName).name + '.json';
 
-    for (let i = 1, ilen = lines.length; i < ilen; i += 1) { // ignore the first header line
-      let item = {};
-      let currentLine = lines[i].split(','); // break line (row) into columns
-
-      for (let x = 1, xlen = headers.length; x < xlen; x += 1) {
-        item[headers[x]] = currentLine[x];
-      }
-
-      results.push(item);
-    }
-
-
-    const jsonFileName = path.parse(fileName).name + '.json';
-
-    // ensure JSON results are stringified
-    fs.writeFile(path.join(__dirname, jsonFileName), JSON.stringify(results), (err) => {
+    fs.writeFile(path.join(__dirname, jsonFilename), jsonContent, (err) => {
       if (err) {
-        console.log('ERROR writing the output JSON file.');
-        console.log(err.message);
+        console.log(`ERROR writing the output JSON file. \n ${err.message}`);
       }
 
       console.log('file conversion completed');
     });
   });
+}
+
+const createJSON = (dataRows, dataHeaders) => {
+  let results = [];
+
+  for (let i = 1, ilen = dataRows.length; i < ilen; i += 1) { // ignore the first header line
+    let item = {};
+    let dataColumns = dataRows[i].split(','); // break line (row) into columns
+
+    for (let x = 1, xlen = dataHeaders.length; x < xlen; x += 1) {
+      item[dataHeaders[x]] = dataColumns[x];
+    }
+
+    results.push(item);
+  }
+
+  return JSON.stringify(results);
 }
 
 csvToJSON(process.argv[2]);
